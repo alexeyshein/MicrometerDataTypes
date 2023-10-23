2f1a9689da356a133cca11802c3f80b8d83ce358
Aleksey Shein <a.shein@riftek.com>
2023-10-23 17:06:15 +0300

    Add RotatedRect and functions Point2d

--- lib/include/Point2d.h
+++ lib/include/Point2d.h
@@ -1,16 +1,17 @@
 ﻿#pragma once
 
+#include <cmath>
 
 namespace rf
 {
 
-template<typename T>
+template<typename _Tp>
 struct Point2d
 {
-	T x;
-	T y;
+	_Tp x;
+	_Tp y;
 	Point2d() {};
-	Point2d(T _x, T _y):
+	Point2d(_Tp _x, _Tp _y):
 		x (_x)
 	,   y(_y)
 	{}
@@ -22,6 +23,192 @@ struct Point2d
 	bool operator !=(const Point2d &other){
        return (x!=other.x) || (y!=other.y);
 	}
+	//! dot product
+    _Tp dot(const Point2d& pt) const;
+    //! dot product computed in double-precision arithmetics
+    double ddot(const Point2d& pt) const;
+    //! cross-product
+    double cross(const Point2d& pt) const;
 };
+
+
+template<typename _Tp> inline
+_Tp Point2d<_Tp>::dot(const Point2d& pt) const
+{
+    return static_cast<_Tp>(x*pt.x + y*pt.y);
+}
+
+template<typename _Tp> inline
+double Point2d<_Tp>::ddot(const Point2d& pt) const
+{
+    return (double)x*(double)(pt.x) + (double)y*(double)(pt.y);
+}
+
+template<typename _Tp> inline
+double Point2d<_Tp>::cross(const Point2d& pt) const
+{
+    return (double)x*pt.y - (double)y*pt.x;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator += (Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    a.x += b.x;
+    a.y += b.y;
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator -= (Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    a.x -= b.x;
+    a.y -= b.y;
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator *= (Point2d<_Tp>& a, int b)
+{
+    a.x = static_cast<_Tp>(a.x * b);
+    a.y = static_cast<_Tp>(a.y * b);
+    return a;
+}
+template<typename _Tp> static inline
+Point2d<_Tp>& operator *= (Point2d<_Tp>& a, float b)
+{
+    a.x = static_cast<_Tp>(a.x * b);
+    a.y = static_cast<_Tp>(a.y * b);
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator *= (Point2d<_Tp>& a, double b)
+{
+    a.x = static_cast<_Tp>(a.x * b);
+    a.y = static_cast<_Tp>(a.y * b);
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator /= (Point2d<_Tp>& a, int b)
+{
+    a.x = static_cast<_Tp>(a.x / b);
+    a.y = static_cast<_Tp>(a.y / b);
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator /= (Point2d<_Tp>& a, float b)
+{
+    a.x = static_cast<_Tp>(a.x / b);
+    a.y = static_cast<_Tp>(a.y / b);
+    return a;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp>& operator /= (Point2d<_Tp>& a, double b)
+{
+    a.x = static_cast<_Tp>(a.x / b);
+    a.y = static_cast<_Tp>(a.y / b);
+    return a;
+}
+
+
+template<typename _Tp> static inline
+double norm(const Point2d<_Tp>& pt)
+{
+    return std::sqrt((double)pt.x*pt.x + (double)pt.y*pt.y);
+}
+
+template<typename _Tp> static inline
+bool operator == (const Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    return a.x == b.x && a.y == b.y;
+}
+
+template<typename _Tp> static inline
+bool operator != (const Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    return a.x != b.x || a.y != b.y;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator + (const Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(a.x + b.x), static_cast<_Tp>(a.y + b.y) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator - (const Point2d<_Tp>& a, const Point2d<_Tp>& b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(a.x - b.x), static_cast<_Tp>(a.y - b.y) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator - (const Point2d<_Tp>& a)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(-a.x), static_cast<_Tp>(-a.y) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (const Point2d<_Tp>& a, int b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(a.x*b), static_cast<_Tp>(a.y*b) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (int a, const Point2d<_Tp>& b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(b.x*a), static_cast<_Tp>(b.y*a) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (const Point2d<_Tp>& a, float b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(a.x*b), static_cast<_Tp>(a.y*b) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (float a, const Point2d<_Tp>& b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(b.x*a), static_cast<_Tp>(b.y*a) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (const Point2d<_Tp>& a, double b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(a.x*b), static_cast<_Tp>(a.y*b) );
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator * (double a, const Point2d<_Tp>& b)
+{
+    return Point2d<_Tp>( static_cast<_Tp>(b.x*a), static_cast<_Tp>(b.y*a) );
+}
+
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator / (const Point2d<_Tp>& a, int b)
+{
+    Point2d<_Tp> tmp(a);
+    tmp /= b;
+    return tmp;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator / (const Point2d<_Tp>& a, float b)
+{
+    Point2d<_Tp> tmp(a);
+    tmp /= b;
+    return tmp;
+}
+
+template<typename _Tp> static inline
+Point2d<_Tp> operator / (const Point2d<_Tp>& a, double b)
+{
+    Point2d<_Tp> tmp(a);
+    tmp /= b;
+    return tmp;
+}
 }
 
---
+++ lib/include/RotatedRect.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <vector>
+
+#include "Point2d.h"
+#include "Rect.h"
+namespace rf
+{
+
+	class RotatedRect
+	{
+	public:
+		RotatedRect();
+		RotatedRect(const Point2d<double> &center, double width, double height, double angle);
+		RotatedRect(const Point2d<double> &point1, const Point2d<double> &point2, const Point2d<double> &point3);
+
+		~RotatedRect() {}
+
+		void Set(const Point2d<double> &center, double width, double height, double angle);
+		void Set(const Point2d<double> &point1, const Point2d<double> &point2, const Point2d<double> &point3);
+		void SetWidth(double width_) { width = width_; }
+		void SetHeight(double height_) { height = height_; }
+        void SetCenter(const Point2d<double> &center_) { center = center_; }
+
+		Point2d<double> Center() const { return center; };
+		double Width() const { return width; }
+		double Height() const { return height; }
+		//in radian
+        double Angle() const { return angle; }
+        std::vector<Point2d<double>> RectPoints();
+        Rect BoundingRect ();
+	private:
+		Point2d<double> center; // center
+		double width;			 // right bottom point
+		double height;
+		double angle;  // angle in Degree
+	};
+
+}
